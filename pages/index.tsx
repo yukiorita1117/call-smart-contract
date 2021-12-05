1a3c3f4fe98e5c4b14ef349b244803f454574923
折田幸夫 <3204704@gmail.com>
2021-12-05 18:53:31 +0900

    feat useWeb3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
 import Layout from "../components/Layout";
 import { Box } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+import { useEagerConnect, useInactiveListener } from "../hooks/useWeb3";
+
+const { connector, library, active } = useWeb3React<Web3Provider>();
+
+const [activatingConnector, setActivatingConnector] = useState<any>();
+useEffect(() => {
+  if (activatingConnector && activatingConnector === connector) {
+    setActivatingConnector(undefined);
+  }
+}, [activatingConnector, connector]);
+
+const triedEager = useEagerConnect();
+
+useInactiveListener(!triedEager || !!activatingConnector);
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
