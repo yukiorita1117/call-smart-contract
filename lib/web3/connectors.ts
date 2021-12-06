f42e44a9711c2f3489b14c7bc6623e4e054746b7
折田幸夫 <3204704@gmail.com>
2021-12-07 00:02:13 +0900

    add comment

--- lib/web3/connectors.ts
+++ lib/web3/connectors.ts
@@ -1,6 +1,7 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { TorusConnector } from "@web3-react/torus-connector";
 
+// 質問 これ何してる？
 export const injected = new InjectedConnector({
   supportedChainIds: [137],
 });
