cbb03ae9adcfe8a2d0b1365232e9cdca6cfeec4e
折田幸夫 <3204704@gmail.com>
2021-12-08 20:56:35 +0900

    remove func

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,6 @@ import { Web3Provider } from "@ethersproject/providers";
 import { useEagerConnect, useInactiveListener } from "../hooks/useWeb3";
 
 const IndexPage = () => {
-  const { connector, library, active } = useWeb3React<Web3Provider>();
-
-  // TODO 何してるん？？？？？
-  const [activatingConnector, setActivatingConnector] = useState<any>();
-  useEffect(() => {
-    if (activatingConnector && activatingConnector === connector) {
-      setActivatingConnector(undefined);
-    }
-  }, [activatingConnector, connector]);
-
-  // TODO 何してるん？？？？？
-  const triedEager = useEagerConnect();
-
-  // TODO 何してるん？？？？？
-  useInactiveListener(!triedEager || !!activatingConnector);
-
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <Box display="flex" justifyContent="center" alignContent="center">
